0d5eb7bc2647d1b0591da942b7a4efa1846b3e9e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-17 16:21:02 +0100

    remove bg

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                   Password
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="Email" />
+                <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="Email" />
                 <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
               </div>
               
@@ -34,7 +34,7 @@ export default function Home() {
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                   Password
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Password" />
+                <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Password" />
                 <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
               </div>
             </div>
