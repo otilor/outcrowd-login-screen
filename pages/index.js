5309e532144b9d30d531ddd361bf9cc619f6373c
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-17 16:14:15 +0100

    add input fields

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,16 @@ export default function Home() {
 
             <hr />
 
-            <div>
-              <input placeholder='Email'/>
-              <input placeholder='Password'/>
+            <div className='pt-2'>
+              <div className='mb-2'>
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+              </div>
+              
+              <div className='mb-2'>
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Second name</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+              </div>
             </div>
 
             <div className='flex justify-between items-center m-5'>
